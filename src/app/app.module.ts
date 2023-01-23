3079564947cfc3acacaddb908cd437733e2f0149
Thea-zin <bansothea20@kit.edu.kh>
2023-01-23 17:49:37 +0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { SiginComponent } from './auth/sigin/sigin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { NavbarComponent } from './user/navbar/navbar.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    SiginComponent,
+    SignupComponent,
+    NavbarComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
